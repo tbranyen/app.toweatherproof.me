00db0232f62eea938dcf8099ff097ad173803ea8
Tim Branyen <tim@tabdeveloper.com>
2013-09-11 13:08:03 -0400

    Bumped up timeout to 5 seconds.

--- source/views/form.js
+++ source/views/form.js
@@ -38,8 +38,8 @@ var FormView = Backbone.View.extend({
         }.bind(this),
 
         {
-          // If a location is not found after a second, timeout.
-          timeout: 1000
+          // If a location is not found after five seconds, timeout.
+          timeout: 5000
         }
       );
     }
